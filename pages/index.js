fbda2af359bbe7fa11c5ff12319840588c0fdbe0
Arnold Kibira <arnold.ril3y@gmail.com>
2021-05-02 00:35:16 +0300

    render all components on the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Content from '../components/Content'
 import Header from '../components/Header'
 import Navigation from '../components/Navigation'
+import requests from '../utils/requests'
 
-export default function Home() {
+export default function Home({ results }) {
+
   return (
     <div>
       <Head>
@@ -12,11 +15,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      {/* navbar */}
       <Navigation />
-      {/* content */}
-      {/* content */}
-
+      <Content results={results} />
     </div>
   )
+}
+
+export const getServerSideProps = async (context) => {
+  const genre = context.query.genre
+
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`).then(res => res.json())
+  return {
+    props: {
+      results: request.results
+    }
+  }
 }
